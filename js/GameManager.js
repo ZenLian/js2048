f5cf7cdac3ac83540b90b873ada2b67c91e0f178
Zenlian <zeninglian@gmail.com>
2020-06-05 22:21:34 +0800

    check point

--- js/GameManager.js
+++ js/GameManager.js
@@ -22,14 +22,38 @@ class GameManager {
     // TODO: do moving
     move(motion) {
         // TODO: get merge direction according to motion
+        var traversals = this.buildTraversals(motion);
+        traversals.x.forEach((x) => {
+            traversals.y.forEach((y) => {
+                var position = { x: x, y: y };
+                var tile = this.grid.CellContent(position);
+                if (tile) {
+                    var lastTile = this.findLastTile(
+                        position,
+                        traversals.vector
+                    );
+                    // situation 1: first tile
+                    if (lastTile === tile) {
+                        continue;
+                    }
+                    if (
+                        lastTile &&
+                        lastTile.value === tile.value &&
+                        !lastTile.mergedFrom
+                    ) {
+                    }
+                }
+            });
+        });
+    }
 
-        // move left
-        if (motion === "left") {
-            this.moveleft();
+    addRandomTile() {
+        if (this.grid.cellsAvailable()) {
+            let value = Math.random() < 0.9 ? 2 : 4;
+            this.grid.addRandomTile(value);
         }
     }
 
-    moveleft() {}
-
-    addRandomTile() {}
+    //TODO
+    buildTraversals(motion) {}
 }
